fbcce1b6174ca84bc216ba7ad37241bb8644377e
Ernanni F <ernannicf@gmail.com>
2021-04-22 01:24:40 -0300

    SSG vs SSR

---
+++ src/pages/index.tsx
@@ -0,0 +1,50 @@
+//SPA
+/*
+import { useEffect } from 'react';
+
+export default function Home() {
+  useEffect(() => {
+    [...]
+  }, []);
+}
+*/
+//SSR
+/*
+export async function getServerSideProps() {
+  [...]
+  return {
+    props: {
+      [...]
+    }
+  }
+}
+*/
+//SSG
+/*
+export async function getServerSideProps() {
+  [...]
+  return {
+    props: {
+      [...]
+    },
+    revalidate: [...]
+  }
+}
+*/
+
+export default function Home(props) {
+  console.log(props.episodes);
+  return <h1>Index</h1>;
+}
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes');
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  };
+}
