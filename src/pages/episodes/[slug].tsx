96d91827d03863c129ff48acaa08000763a931aa
Ernanni F <ernannicf@gmail.com>
2021-04-25 16:08:13 -0300

    detail page

---
+++ src/pages/episodes/[slug].tsx
@@ -0,0 +1,97 @@
+import { format, parseISO } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import Image from 'next/image';
+import Link from 'next/link';
+import { GetStaticPaths, GetStaticProps } from 'next';
+import { useRouter } from 'next/router';
+
+import { api } from '../../services/api';
+import { convertDurationToTimeString } from '../../utils/convertDurationToTimeString';
+
+import styles from './episode.module.scss';
+
+interface IEpisode {
+  id: string;
+  title: string;
+  members: string;
+  publishedAt: string;
+  thumbnail: string;
+  description: string;
+  url: string;
+  duration: number;
+  durationAsString: string;
+}
+
+interface IEpisodeProps {
+  episode: IEpisode;
+}
+
+export default function Episode({ episode }: IEpisodeProps) {
+  const router = useRouter();
+
+  return (
+    <div className={styles.episode}>
+      <div className={styles.thumbnailContainer}>
+        <Link href='/'>
+          <button type='button'>
+            <img src='/arrow-left.svg' alt='Voltar' />
+          </button>
+        </Link>
+        <Image
+          width={700}
+          height={160}
+          src={episode.thumbnail}
+          objectFit='cover'
+        />
+        <button type='button'>
+          <img src='/play.svg' alt='Tocar episódio' />
+        </button>
+      </div>
+
+      <header>
+        <h1>{episode.title}</h1>
+        <span>{episode.members}</span>
+        <span>{episode.publishedAt}</span>
+        <span>{episode.durationAsString}</span>
+      </header>
+
+      <div
+        className={styles.description}
+        dangerouslySetInnerHTML={{ __html: episode.description }}
+      />
+    </div>
+  );
+}
+
+export const getStaticPaths: GetStaticPaths = async (context) => {
+  return {
+    paths: [],
+    fallback: 'blocking',
+  };
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const { slug } = context.params;
+  const { data } = await api.get(`episodes/${slug}`);
+
+  const episode = {
+    id: data.id,
+    title: data.title,
+    thumbnail: data.thumbnail,
+    members: data.members,
+    publishedAt: format(parseISO(data.published_at), 'd MMM yy', {
+      locale: ptBR,
+    }),
+    duration: Number(data.file.duration),
+    durationAsString: convertDurationToTimeString(Number(data.file.duration)),
+    description: data.description,
+    url: data.file.url,
+  };
+
+  return {
+    props: {
+      episode,
+    },
+    revalidate: 60 * 60 * 24,
+  };
+};
